9f8e45c02fce6c3832941d0126aa4203edad508b
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-15 22:13:45 +0200

    Added Event Model.

---
+++ models/event.js
@@ -0,0 +1,16 @@
+export default function Event() {
+    return {
+        start :[2021,5,13,6,0],
+        duration : {},
+        title:'',
+        description:'',
+        location:'',
+        url:'',
+        geo:{},
+        categories:[],
+        status:'',
+        busyStatus:'',
+        organizer:{},
+        attendees:[]
+    }
+}
